9306e37b871f05dc7bd2ab1f82ba86b0e1bdc038
Diryfort <dorhungmi2@gmail.com>
2023-11-01 09:58:26 +0300

    Initial commit

---
+++ app/src/main/java/com/maxim/getdatafromapiandshow2/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.maxim.getdatafromapiandshow2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val actionButton = findViewById<Button>(R.id.actionButton)
+
+
+    }
+}
